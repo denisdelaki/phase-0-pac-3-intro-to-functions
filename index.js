c72c5404e76e9ffbf5f586b16d9fc92fd024d92e
denisdelaki <denisdelaki@gmail.com>
2022-06-11 15:10:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Follow along with the examples here
+/*function doNothing() {}
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
+*/
+function add(x, y) {
+  return x + y;
+}
+console.log(add(1, 2));
